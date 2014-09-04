1399085babed400e823be193bab2321d4ad45cb8
Jean Schurger <jean@schurger.org>
2014-09-04 17:56:12 -0400

    PEP8, better /auth.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import time
-import hmac, hashlib
+import hmac
+import hashlib
 import base64
 
 from flask import Flask, render_template
@@ -8,21 +9,24 @@ app = Flask(__name__)
 
 app.config.from_pyfile("app.cfg")
 
+
 def createKey(username, secret, expires):
     return base64.encodestring(hmac.HMAC(
         secret, str(expires) + ":" + username,
         hashlib.sha1).digest()).replace(' ', '+').strip()
 
+
 @app.route("/auth")
 def auth():
     expires = int(time.time() + 300)
-    key = createKey(app.config['USERNAME']), app.config['SECRET'], expires)
-    return 'var voxrtc_config = ' \
-           '{"key": "%s", "expires": %d, "username": "%s"};' % (
-        key, expires, app.config['USERNAME'])
+    key = createKey(app.config['USERNAME'], app.config['SECRET'], expires)
+    return 'var voxrtc_config = %s;' % str(
+        {"key": key, "expires": expires,
+         "username": app.config['USERNAME']})
+
 
 @app.route("/demo")
-def index():    
+def index():
     return render_template("index.html")
 
 app.run(host='0.0.0.0', port=5001)
